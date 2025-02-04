ebf33b4fe0125dbc2d4a9c792b7c876862bc0e11
Denis Neves <nevesdenis35@gmail.com>
2025-02-04 11:41:21 -0300

    Condicional simples

---
+++ 05-condicionais.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>05 - Condicionais PHP</title>
+</head>
+
+<body>
+  <h1>Estruturas Condicionais</h1>
+  <hr>
+
+  <h2>Simples
+    <h3><code>if</code></h3>
+    <?php
+    $numero = 5;
+    if ($numero > 1) {
+      echo "<p>$numero é maior que 1</p>";
+    }
+
+    // Sintaxe omitindo as chaves
+    if ($numero > 1) echo "<p>$numero é maior que 1</p>";
+    ?>
+
+    <hr>
+
+
+    <h2>Compostas
+      <?php
+      ?>
+      <hr>
+
+
+      <h2>Encadeada
+        <?php
+        ?>
+</body>
+
+</html>
