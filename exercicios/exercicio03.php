df59e67355709e5d5656edb457bc2f88dde38418
Denis Neves <nevesdenis35@gmail.com>
2025-02-05 10:39:46 -0300

    Cria exercicio de condicionais

---
+++ exercicios/exercicio03.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercicio 03 - Condicionais</title>
+</head>
+
+<body>
+  <h1>Exercicio 03 - Trabalhando com condicionais</h1>
+
+</body>
+
+</html>
