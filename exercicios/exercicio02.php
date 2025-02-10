a051cdeaaab86450f1e0eddd735dfd68b727d893
Denis Neves <nevesdenis35@gmail.com>
2025-02-10 11:27:57 -0300

    Formata exercicios com bootstap

--- exercicios/exercicio02.php
+++ exercicios/exercicio02.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Exercício 02 PHP e HTML: Arrays</title>
 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- 
   <style>
     * {
       font-family: sans-serif;
@@ -64,7 +67,7 @@
       flex-direction: column;
       gap: .5rem;
     }
-  </style>
+  </style> -->
 </head>
 
 <body>
@@ -142,13 +145,13 @@ Desafio: destaque estes dois blocos de conteúdo usando CSS e mostre-os lado a l
     </div>
   </article> -->
 
-  <article>
-    <h2>Usuários</h2>
+  <article class="container">
+    <h2 class="text-center text-uppercase">Usuários</h2>
 
-    <div class="users">
-      <section>
-        <h3>Usuário 1</h3>
-        <ul>
+    <div class="row p-2">
+      <section class="col ">
+        <h3 class="text-center">Usuário 1</h3>
+        <ul class="rounded-3 px-4 py-2 bg-info-subtle">
           <li>Nome: <?= $users["ciclano"]["nome"] ?></li>
           <li>Email: <?= $users["ciclano"]["email"] ?></li>
           <li>idade: <?= $users["ciclano"]["idade"] ?></li>
@@ -157,9 +160,9 @@ Desafio: destaque estes dois blocos de conteúdo usando CSS e mostre-os lado a l
       </section>
 
 
-      <section>
-        <h3>Usuário 2</h3>
-        <ul>
+      <section class="col ">
+        <h3 class="text-center">Usuário 2</h3>
+        <ul class="rounded-3 px-4 py-2 bg-info">
           <li>Nome: <?= $users["fulano"]["nome"] ?></li>
           <li>Email: <?= $users["fulano"]["email"] ?></li>
           <li>Idade: <?= $users["fulano"]["idade"] ?></li>
@@ -168,6 +171,10 @@ Desafio: destaque estes dois blocos de conteúdo usando CSS e mostre-os lado a l
       </section>
     </div>
   </article>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- exercicios/exercicio04.php
+++ exercicios/exercicio04.php
@@ -5,91 +5,51 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Exercício 04 - Loop e Estrutura de Dados</title>
-  <style>
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    body {
-      font-family: sans-serif;
-      height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-
-    table {
-      width: 100%;
-      max-width: 50%;
-      border-collapse: collapse;
-    }
-
-    th,
-    td {
-      padding: .875rem;
-      text-align: left;
-      border: 1px solid #ddd;
-    }
-
-    th {
-      background-color: #4c7faf;
-      color: white;
-    }
-
-    tr:nth-child(even) {
-      background-color: #f2f2f2;
-    }
-
-    tr:nth-child(odd) {
-      background-color: #ffffff;
-    }
-
-    tr:hover {
-      background-color: #ddd;
-    }
-  </style>
 </head>
 
 <body>
-  <h1>Exercício 04 - Loop e Estrutura de Dados</h1>
+  <h1 class="text-center">Exercício 04 - Loop e Estrutura de Dados</h1>
   <hr>
-  <?php
-  $dados = [
-    "HTML" => "Estruturação",
-    "CSS" => "Estilos",
-    "JS" => "Comportamentos",
-    "PHP" => "Back-End",
-    "SQL" => "Manipulação de dados",
-    "Java" => "Softwares"
-  ];
-  ?>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Linguagem</th>
-        <th>Descrição</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      $i = 1;
-      foreach ($dados as $key => $value) {
-      ?>
+  <div class="container d-flex flex-column align-items-center justify-content-center ">
+    <?php
+    $dados = [
+      "HTML" => "Estruturação",
+      "CSS" => "Estilos",
+      "JS" => "Comportamentos",
+      "PHP" => "Back-End",
+      "SQL" => "Manipulação de dados",
+      "Java" => "Softwares"
+    ];
+    ?>
+    <table class="table table-striped-columns table-hover w-50">
+      <thead class="table-success">
         <tr>
-          <td><?= $i ?></td>
-          <td><?= $key ?></td>
-          <td><?= $value ?></td>
+          <th>ID</th>
+          <th>Linguagem</th>
+          <th>Descrição</th>
         </tr>
-      <?php
-        $i++;
-      }
-      ?>
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        <?php
+        $i = 1;
+        foreach ($dados as $key => $value) {
+        ?>
+          <tr>
+            <td><?= $i ?></td>
+            <td><?= $key ?></td>
+            <td><?= $value ?></td>
+          </tr>
+        <?php
+          $i++;
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 
