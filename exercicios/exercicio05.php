c86492a0a1c870db96a14e020b6eb05ae0362b3c
Denis Neves <nevesdenis35@gmail.com>
2025-02-11 17:36:49 -0300

    altera for para forEach e formata com bootstrap

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
@@ -10,7 +10,7 @@
 
 <body>
   <div class="container">
-    <h1>Exercicio 05 - Funções</h1>
+    <h1 class="text-center text-dark">Exercicio 05 - Funções</h1>
     <hr>
 
     <?php
@@ -26,23 +26,55 @@
       return $calcMediaFormatado;
     };
 
-    $valorCalcMedia = $calcularMedia(8, 8, 8);
+    // $valorCalcMedia = $calcularMedia(4, 4, 4);
 
+    //função que verifica se o aluno está aprovado/reprovado  com base no valor da média
     function verificaSituacao(float $valorCalcMedia): string
     {
       if ($valorCalcMedia >= 7) {
-        return "Aprovado";
+        $situacao = "<p class='alert alert-success'>Você está aprovado(a). <b>Sua média é: $valorCalcMedia</b></p>";
       } else {
-        return "Reprovado";
+        $situacao = "<p class='alert alert-danger'>Você está reprovado(a). <b>Sua média é: $valorCalcMedia</b></p>";
       }
+      return $situacao;
     }
-    verificaSituacao($valorCalcMedia);
 
+    //echo pra veirificar a saída da função que verifica a situação do aluno
+    // echo verificaSituacao($valorCalcMedia);
+    
+    $alunos = [
+      "Denis"=>[10,7.5,6.3],
+      "Tenis"=>[6.9,6.9,6.9],
+      "Penis"=>[6,6,6],
+      "Zenis"=>[10,10,10],
+      "Menis"=>[10,10,10],
+    ];
+    
+    // $keys = array_keys($alunos);
+
+    // for ($i = 0; $i < count($keys); $i++) {
+      // $aluno = $keys[$i];
+      // $notas = $alunos[$aluno];
     ?>
+    <div class="d-flex flex-wrap gap-3">
+    <?php
+    foreach ($alunos as $aluno => $notas) {
+      $valorCalcMedia = $calcularMedia($notas[0], $notas[1], $notas[2]);
+      ?>
 
-    <p class="alert alert-success">Você está <?= verificaSituacao($valorCalcMedia) ?></p>
+      <div class="alert alert-warning flex-fill">
+        <h3>Aluno(a): <?= $aluno?></h3>
+        <p><?= verificaSituacao($valorCalcMedia)?></p>
+      </div>
+    <?php
+    }
+    ?>
+    </div>
+
+    <!-- <div class="situacao">
+      <?=verificaSituacao($valorCalcMedia)?>
+    </div> -->
   </div>
-
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
