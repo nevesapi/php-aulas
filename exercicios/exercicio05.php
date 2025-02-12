dd26df5064e58f1a3c40f1c0c194ab7716eb1345
Denis Neves <nevesdenis35@gmail.com>
2025-02-12 10:46:44 -0300

    Matriz de matrizes kkkkk

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -41,39 +41,60 @@
 
     //echo pra veirificar a saída da função que verifica a situação do aluno
     // echo verificaSituacao($valorCalcMedia);
-    
-    $alunos = [
-      "Denis"=>[10,7.5,6.3],
-      "Tenis"=>[6.9,6.9,6.9],
-      "Penis"=>[6,6,6],
-      "Zenis"=>[10,10,10],
-      "Menis"=>[10,10,10],
-    ];
-    
-    // $keys = array_keys($alunos);
 
-    // for ($i = 0; $i < count($keys); $i++) {
-      // $aluno = $keys[$i];
-      // $notas = $alunos[$aluno];
+    $alunos = [
+      [
+        "nome" => "Denis",
+        "nota1" => 3,
+        "nota2" => 8,
+        "nota3" => 2,
+      ],
+      [
+        "nome" => "Paulo",
+        "nota1" => 9,
+        "nota2" => 8,
+        "nota3" => 7,
+      ],
+      [
+        "nome" => "Heloisa",
+        "nota1" => 6,
+        "nota2" => 8,
+        "nota3" => 7,
+      ],
+      [
+        "nome" => "Gabriel",
+        "nota1" => 10,
+        "nota2" => 8,
+        "nota3" => 7,
+      ],
+      [
+        "nome" => "Maycon",
+        "nota1" => 10,
+        "nota2" => 8,
+        "nota3" => 7,
+      ],
+      [
+        "nome" => "Júlia",
+        "nota1" => 10,
+        "nota2" => 8,
+        "nota3" => 7,
+      ],
+    ];
     ?>
     <div class="d-flex flex-wrap gap-3">
-    <?php
-    foreach ($alunos as $aluno => $notas) {
-      $valorCalcMedia = $calcularMedia($notas[0], $notas[1], $notas[2]);
+      <?php
+      foreach ($alunos as $aluno) {
+        $valorCalcMedia = $calcularMedia($aluno["nota1"], $aluno["nota2"], $aluno["nota3"]);
       ?>
 
-      <div class="alert alert-warning flex-fill">
-        <h3>Aluno(a): <?= $aluno?></h3>
-        <p><?= verificaSituacao($valorCalcMedia)?></p>
-      </div>
-    <?php
-    }
-    ?>
+        <div class="alert alert-warning flex-fill">
+          <h3>Aluno(a): <?= $aluno["nome"] ?></h3>
+          <?= verificaSituacao($valorCalcMedia) ?>
+        </div>
+      <?php
+      }
+      ?>
     </div>
-
-    <!-- <div class="situacao">
-      <?=verificaSituacao($valorCalcMedia)?>
-    </div> -->
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
