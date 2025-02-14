09acaa6ec383567a18b04b8edffe9c2d06d41b05
Denis Neves <nevesdenis35@gmail.com>
2025-02-14 08:29:21 -0300

    index e conteudo

---
+++ 11-site/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP | Site com bootstrap</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container">
+    <header>
+      <h1>Logo</h1>
+      <nav>
+        <a href="index.php">Home</a>
+        <a href="cursos.php">Cursos</a>
+        <a href="duvidas.php">Dúvidas</a>
+      </nav>
+    </header>
+    <main>
+      <h2>Bem comido a página Home</h2>
+      <p>Primeira página do site</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit cumque nobis perferendis ducimus dolores impedit necessitatibus fugit natus qui cupiditate eius reiciendis, corporis debitis quas ipsam cum aperiam eveniet. Fugit!</p>
+    </main>
+    <footer>
+      <p>Site criado com worpress.</p>
+    </footer>
+
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
