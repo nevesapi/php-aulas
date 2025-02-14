94c6e69bf5941c50a3992e4a2c9978c9f33d524f
Denis Neves <nevesdenis35@gmail.com>
2025-02-14 08:53:07 -0300

    cria e adiciona component head

--- 11-site/cursos.php
+++ 11-site/cursos.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require "cabecalho.php" ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP | Cursos - Site com bootstrap</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-  <div class="container">
-    <header>
-      <h1>Logo</h1>
-      <nav>
-        <a href="index.php">Home</a>
-        <a href="cursos.php">Cursos</a>
-        <a href="duvidas.php">Dúvidas</a>
-      </nav>
-    </header>
-    <main>
       <h2>Página Cursos</h2>
       <p>Veja os cursos disponiveis</p>
       <ul>
--- 11-site/duvidas.php
+++ 11-site/duvidas.php
@@ -1,48 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require "cabecalho.php" ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP | Dúvidas - Site com bootstrap</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
+<h2>Página Dúvidas</h2>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci sit beatae quam! Dolorum labore nesciunt, maiores sed dicta illum eveniet exercitationem provident voluptates inventore distinctio enim recusandae, magnam ducimus? Sint.</p>
 
-<body>
-  <div class="container">
-    <header>
-      <h1>Logo</h1>
-      <nav>
-        <a href="index.php">Home</a>
-        <a href="cursos.php">Cursos</a>
-        <a href="duvidas.php">Dúvidas</a>
-      </nav>
-    </header>
+<h3>Principais dúvidas</h3>
+<details>
+  <summary>Como se cadastrar?</summary>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptates animi illo harum officiis quos quasi. Similique obcaecati dolore, nesciunt eos asperiores explicabo aliquam dolor omnis molestias suscipit, sint eveniet?</p>
+</details>
 
-    <main>
-      <h2>Página Dúvidas</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci sit beatae quam! Dolorum labore nesciunt, maiores sed dicta illum eveniet exercitationem provident voluptates inventore distinctio enim recusandae, magnam ducimus? Sint.</p>
+<details>
+  <summary>Como se cadastrar?</summary>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptates animi illo harum officiis quos quasi. Similique obcaecati dolore, nesciunt eos asperiores explicabo aliquam dolor omnis molestias suscipit, sint eveniet?</p>
+</details>
+</main>
 
-      <h3>Principais dúvidas</h3>
-      <details>
-        <summary>Como se cadastrar?</summary>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptates animi illo harum officiis quos quasi. Similique obcaecati dolore, nesciunt eos asperiores explicabo aliquam dolor omnis molestias suscipit, sint eveniet?</p>
-      </details>
+<footer>
+  <p>Site criado com worpress.</p>
+</footer>
 
-      <details>
-        <summary>Como se cadastrar?</summary>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptates animi illo harum officiis quos quasi. Similique obcaecati dolore, nesciunt eos asperiores explicabo aliquam dolor omnis molestias suscipit, sint eveniet?</p>
-      </details>
-    </main>
-
-    <footer>
-      <p>Site criado com worpress.</p>
-    </footer>
-
-  </div>
+</div>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
